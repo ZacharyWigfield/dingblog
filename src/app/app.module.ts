3142262b4f6029a0ca3921b3e8dc9462d6f0e65a
Zachary Wigfield <zewigfield@gmail.com>
2021-12-21 03:47:12 -0500

    created reusable submit button and implemented it across multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BlogWriteComponent } from './blog-write/blog-write.component';
 import { BlogReadComponent } from './blog-read/blog-read.component';
 import { LoginComponent } from './login/login.component';
+import { SubmitBtnComponent } from './shared/submit-btn/submit-btn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogWriteComponent,
     BlogReadComponent,
-    LoginComponent
+    LoginComponent,
+    SubmitBtnComponent
   ],
   imports: [
     BrowserModule,
